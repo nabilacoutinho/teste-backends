cf729dea99d73caf9a591152acea9ccd26b7e5a0
Nabila <nabilasaturnina@gmail.com>
2020-03-16 20:52:30 -0300

    add proposal validation

--- java/model/Proposal.java
+++ java/model/Proposal.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Proposal {
+
+	private final static int LOAN_VALUE_MIN_LIMIT = 30000;
+	private final static int LOAN_VALUE_MAX_LIMIT = 3000000;
+	private final static int INSTALLMENTS_MIN_LIMIT = 24;
+	private final static int INSTALLMENTS_MAX_LIMIT = 180;
 	
 	private String id;
 	private double loanValue;
@@ -14,6 +19,18 @@ public class Proposal {
 	public Proposal() {
 		this.warranties = new ArrayList<Warranty>();
 		this.proponents = new ArrayList<Proponent>();
+	}
+	
+	public boolean isValid() {
+		return isValidLoanValue() && isValidInstallmentsQty();
+	}
+
+	private boolean isValidLoanValue() {
+		return this.loanValue >= LOAN_VALUE_MIN_LIMIT && this.loanValue <= LOAN_VALUE_MAX_LIMIT;
+	}
+	
+	private boolean isValidInstallmentsQty() {
+		return this.monthlyInstallmentsQty >= INSTALLMENTS_MIN_LIMIT && this.monthlyInstallmentsQty <= INSTALLMENTS_MAX_LIMIT;
 	}
 	
 	public String getId() {
@@ -76,4 +93,50 @@ public class Proposal {
 		this.proponents.remove(proponent);
 	}
 	
+	public static Builder builder() {
+		return new Builder();
+	}
+	
+	public static class Builder {
+		private Proposal proposal;
+		
+		private Builder() {
+			this.proposal = new Proposal();
+		}
+		
+		public Builder id(String id) {
+			this.proposal.id = id;
+			
+			return this;
+		}
+		
+		public Builder loanValue(double loanValue) {
+			this.proposal.loanValue = loanValue;
+			
+			return this;
+		}
+		
+		public Builder monthlyInstallmentsQty(int monthlyInstallmentsQty) {
+			this.proposal.monthlyInstallmentsQty = monthlyInstallmentsQty;
+			
+			return this;
+		}
+		
+		public Builder warranties(List<Warranty> warranties) {
+			this.proposal.warranties = warranties;
+			
+			return this;
+		}
+		
+		public Builder proponents(List<Proponent> proponents) {
+			this.proposal.proponents = proponents;
+			
+			return this;
+		}
+		
+		public Proposal build() {
+			return this.proposal;
+		}
+
+	}
 }
---
+++ java/test/model/ProposalTest.java
@@ -0,0 +1,75 @@
+package test.model;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import model.Proposal;
+
+public class ProposalTest {
+
+	private static final int VALID_LOAN_VALUE = 100000;
+	private static final int VALID_INSTALLMENTS = 30;
+
+	@Test
+	public void testIsValid_WhenAllConditionsAreFine_ThenReturnTrue() {
+		Proposal proposal = Proposal.builder()
+				.loanValue(VALID_LOAN_VALUE)
+				.monthlyInstallmentsQty(VALID_INSTALLMENTS)
+				.build();
+		
+		boolean response = proposal.isValid();
+		
+		assertTrue(response);
+	}
+
+	@Test
+	public void testIsValid_WhenProposalLoanValueAboveLimit_ThenReturnFalse() {
+		Proposal proposal = Proposal.builder()
+				.loanValue(9999999)
+				.monthlyInstallmentsQty(VALID_INSTALLMENTS)
+				.build();
+		
+		boolean response = proposal.isValid();
+		
+		assertFalse(response);
+	}
+
+	@Test
+	public void testIsValid_WhenProposalLoanValueBelowLimit_ThenReturnFalse() {
+		Proposal proposal = Proposal.builder()
+				.loanValue(1)
+				.monthlyInstallmentsQty(VALID_INSTALLMENTS)
+				.build();
+		
+		boolean response = proposal.isValid();
+		
+		assertFalse(response);
+	}
+
+	@Test
+	public void testIsValid_WhenProposalInstallmentIsAboveLimit_ThenReturnFalse() {
+		Proposal proposal = Proposal.builder()
+				.loanValue(VALID_LOAN_VALUE)
+				.monthlyInstallmentsQty(200)
+				.build();
+		
+		boolean response = proposal.isValid();
+		
+		assertFalse(response);
+	}
+
+	@Test
+	public void testIsValid_WhenProposalInstallmentIsBelowLimit_ThenReturnFalse() {
+		Proposal proposal = Proposal.builder()
+				.loanValue(VALID_LOAN_VALUE)
+				.monthlyInstallmentsQty(2)
+				.build();
+		
+		boolean response = proposal.isValid();
+		
+		assertFalse(response);
+	}
+	
+}
